695ac46e151118fe24f9253c0997e038750f302c
Cyakuz <102320084+Cyakuz@users.noreply.github.com>
2023-06-30 01:10:24 +0300

    Panel Deneme CSS Buton

--- src/components/Flow.js
+++ src/components/Flow.js
@@ -1,13 +1,25 @@
 import React from "react";
-import ReactFlow, { Controls, Background } from 'reactflow';
+import ReactFlow, { Controls, Background, Panel } from 'reactflow';
 import 'reactflow/dist/style.css';
+import Button from 'react-bootstrap/Button';
+
+
 
 function Flow() {
+
   return (
     <div style={{ height: '100%' }}>
+      
       <ReactFlow>
+     
+     
+      <Panel position="top-left" name="top-left">
+      <button class="button button5">Black</button>
+      </Panel>
+      
         <Background />
         <Controls />
+        
       </ReactFlow>
     </div>
   );
